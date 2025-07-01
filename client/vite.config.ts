27401a5c42294d3ec31d8aa83df06dd44b57f249
Avinash <avinash.os@tecversantinfoteche>
2025-07-01 20:41:36 +0530

    dockerize

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -13,5 +12,9 @@ export default defineConfig({
   },
   define: {
     'process.env': {}
+  },
+  build: {
+    outDir: 'dist', // 👈 Ensure output goes to /dist for Nginx to serve
+    emptyOutDir: true,
   }
 });
